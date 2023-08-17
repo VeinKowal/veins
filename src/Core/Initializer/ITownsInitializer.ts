9e902f67f0bb3f7502d7dc5587d4bd3a2c0bd2d6
guoweiyu <veinkowal@163.com>
2023-08-17 16:33:39 +0800

    fix: 地球场景销毁后天空场景未去除的问题;

--- src/Core/Initializer/ITownsInitializer.ts
+++ src/Core/Initializer/ITownsInitializer.ts
@@ -102,6 +102,7 @@ class ITownsInitializer extends BaseInitializer {
   public dispose() {
     try {
       const app = this.app;
+      app.renderer.setClearAlpha(0.0);
       app.view = undefined;
       this.view.controls.dispose();
       this.view.dispose();
